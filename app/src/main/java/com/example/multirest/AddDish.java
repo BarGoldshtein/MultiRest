6bd869513e1265bebe4794f0089edf8a78deeeb0
benyamindarmoni <benyamindarmoni@gmail.com>
2019-11-24 19:28:27 +0200

    update dishes

--- app/src/main/java/com/example/multirest/AddDish.java
+++ app/src/main/java/com/example/multirest/AddDish.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.example.multirest.ui.Dish;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -19,10 +20,24 @@ public class AddDish extends AppCompatActivity {
 EditText txtname,txtprice;
 Button btnsave;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    DatabaseReference myRef = database.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dish);
-    }
+        btnsave=(Button) findViewById(R.id.button6);
+        txtname=(EditText)findViewById(R.id.editText);
+        txtprice=(EditText)findViewById(R.id.editText2);
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double p=Double.parseDouble(txtprice.getText().toString());
+                Dish d=new Dish(txtname.getText().toString(),p);
+                myRef.setValue(d);
+            }
+
+
+    });
+
+}
 }
