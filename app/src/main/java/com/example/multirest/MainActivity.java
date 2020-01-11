86c535bd567133ab738f1909a6eae84225ff853a
bar goldshtein <bargggv@gmail.com>
2020-01-11 18:50:39 +0200

    added menu opiton with new about us page (need to finish back buttom)

--- app/src/main/java/com/example/multirest/MainActivity.java
+++ app/src/main/java/com/example/multirest/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.multirest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +45,30 @@ private Button buttonOwner;
         });
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.about_us){
+            Toast.makeText(this, "about us" , Toast.LENGTH_LONG).show();
+
+            Intent intent=new Intent(this,AboutUs.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     public void openClient(){
             Intent intent=new Intent(this,Client.class);
             startActivity(intent);
