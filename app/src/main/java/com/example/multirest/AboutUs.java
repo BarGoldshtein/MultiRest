772b74a3908bfd40fe48959c29d877c8de82e901
bar goldshtein <bargggv@gmail.com>
2020-01-12 12:58:13 +0200

    remove back buttom in about us

--- app/src/main/java/com/example/multirest/AboutUs.java
+++ app/src/main/java/com/example/multirest/AboutUs.java
@@ -2,13 +2,23 @@ package com.example.multirest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutUs extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-    }
+
+
+
+}
+
+
+
+
+
 }
